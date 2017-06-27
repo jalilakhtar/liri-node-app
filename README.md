24bb916ea64fc013146cc36ac3ac307b6a725f8c
jalilakhtar <jalil@meditaz.com>
2017-06-26 17:24:43 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # liri-node-app
+
+# Install these npm packages:
+
+# npm install twitter
+# npm install spotify
+# npm install request
+
+# Command query format to run LIRI.js
+
+# node liri.js my-tweets
+# node liri.js spotify-this-song '<song name here>'
+# node liri.js movie-this '<movie name here>'
+# node liri.js do-what-it-says 
---
+++ keys.js
@@ -0,0 +1,21 @@
+// JavaScript
+console.log('Liri app is loaded');
+
+exports.twitterkeys = {
+	consumer_key: 'xk7jzq9IInO1ncKuwXpDWB9De',
+	consumer_secret: 'DFHi9kevD5R0nVDo0WzwNDGSbOaWxhyY9T86U7sUrK8E5c97xL',
+	access_token_key: '878834854518861826-cQMQCBpANLGEgyLhox8p2STDOWQhWkN',
+	access_token_secret: 'IlGFvPzlr4u1ZOYqZ5n4lfDBI069zISHgD7B1LRtfc5CL',
+}
+// '''
+
+// spotify keys
+// Client ID: 7e7e50db54ad47798606dc042e124a51
+// Client Secret: 50e5a24645b845398947e23b149e2642
+
+exports.spotifykeys = {
+	client_id: '7e7e50db54ad47798606dc042e124a51',
+	client_secret: '50e5a24645b845398947e23b149e2642',
+}
+
+
